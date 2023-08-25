3bcffea6a9fa2d716a22b73b0b8c8d7694d1f03c
Yur-ok <Materik-r@ya.ru>
2023-08-25 21:18:13 +0700

    setup db connection

--- advanced/environments/dev/common/config/main-local.php
+++ advanced/environments/dev/common/config/main-local.php
@@ -4,7 +4,7 @@ return [
     'components' => [
         'db' => [
             'class' => \yii\db\Connection::class,
-            'dsn' => 'psql:host=postgres;dbname=upgrade',
+            'dsn' => 'pgsql:host=postgres;dbname=upgrade',
             'username' => 'root',
             'password' => 'root',
             'charset' => 'utf8',
