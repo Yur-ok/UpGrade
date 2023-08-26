4ce6522f01d6cb1b82757644cd2c5020ea9f4c71
Yur-ok <Materik-r@ya.ru>
2023-08-26 14:24:51 +0700

    install basic template of yii2

---
+++ src/config/db.php
@@ -0,0 +1,16 @@
+<?php
+
+use yii\db\Connection;
+
+return [
+    'class' => Connection::class,
+    'dsn' => 'pgsql:host=postgres;dbname=upgrade',
+    'username' => 'root',
+    'password' => 'root',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
