91098aaf0706e389b29d772b259728bed0a68c1c
Yur-ok <Materik-r@ya.ru>
2023-08-26 14:58:06 +0700

    add index action

--- src/controllers/CrudController.php
+++ src/controllers/CrudController.php
@@ -3,6 +3,7 @@
 namespace app\controllers;
 
 use app\models\Country;
+use yii\data\Pagination;
 use yii\web\Controller;
 
 class CrudController extends Controller
@@ -10,7 +11,21 @@ class CrudController extends Controller
 
     public function actionIndex(): string
     {
-        $countries = Country::findAll([]);
-        return $this->render('index', compact($countries));
+        $query = Country::find();
+
+        $pagination = new Pagination([
+            'defaultPageSize' => 5,
+            'totalCount' => $query->count(),
+        ]);
+
+        $countries = $query->orderBy('name')
+            ->offset($pagination->offset)
+            ->limit($pagination->limit)
+            ->all();
+
+        return $this->render('index', [
+            'countries' => $countries,
+            'pagination' => $pagination,
+        ]);
     }
 }
--- src/migrations/m230826_074452_add_table_country.php
+++ src/migrations/m230826_074452_add_table_country.php
@@ -7,36 +7,40 @@ use yii\db\Migration;
  */
 class m230826_074452_add_table_country extends Migration
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function safeUp()
-    {
-
-    }
+    private string $tableName;
 
     /**
      * {@inheritdoc}
      */
-    public function safeDown()
+    public function up(): void
     {
-        echo "m230826_074452_add_table_country cannot be reverted.\n";
+        $this->tableName = 'country';
+        $this->createTable($this->tableName, [
+                'code' => $this->string(2)->notNull()->unique(),
+                'name' => $this->string(52)->notNull(),
+                'population' => $this->integer(11)->notNull()->defaultValue('0')
+            ]
+        );
 
-        return false;
+        $this->batchInsert($this->tableName, ['code', 'name', 'population'], [
+            ['AU', 'Australia', 24016400],
+            ['BR', 'Brazil', 205722000],
+            ['CA', 'Canada', 35985751],
+            ['CN', 'China', 1375210000],
+            ['DE', 'Germany', 81459000],
+            ['FR', 'France', 64513242],
+            ['GB', 'United Kingdom', 65097000],
+            ['IN', 'India', 1285400000],
+            ['RU', 'Russia', 146519759],
+        ]);
     }
 
-    /*
-    // Use up()/down() to run migration code without a transaction.
-    public function up()
+    /**
+     * {@inheritdoc}
+     */
+    public function down(): void
     {
-
+        $this->dropTable($this->tableName);
     }
 
-    public function down()
-    {
-        echo "m230826_074452_add_table_country cannot be reverted.\n";
-
-        return false;
-    }
-    */
 }
