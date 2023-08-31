7c1a1bda81134217addca9498583efc96dc8cbf1
Yur-ok <Materik-r@ya.ru>
2023-08-31 20:38:28 +0700

    update

--- src/sql/8.3.sql
+++ src/sql/8.3.sql
@@ -1,9 +1,10 @@
-WITH countries AS (SELECT *
-                   FROM customers
-                   WHERE country IN (SELECT country
-                                     FROM customers
-                                     WHERE country NOT LIKE '%a%'))
-SELECT *
-FROM countries
-WHERE city LIKE '%a%'
-ORDER BY country;
+CREATE TABLE simple
+(
+    temp varchar NOT NULL
+)
+
+INSERT INTO simple (temp)
+
+SELECT customer_name
+FROM customers
+;
