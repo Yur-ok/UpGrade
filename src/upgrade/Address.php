6d771cbb94c97da3d1dfa310609bfb871641abb7
Yur-ok <Materik-r@ya.ru>
2024-08-27 19:40:10 +0700

    refactor

--- src/upgrade/Address.php
+++ src/upgrade/Address.php
@@ -1,31 +1,40 @@
 <?php
 
-trait Country {
-    public function getCountryName()
+trait Country
+{
+    public function getName()
     {
         return 'Страна: ' . $this->country;
     }
 }
 
-trait City {
-    public function getCityName()
+trait City
+{
+    public function getName()
     {
         return 'Город: ' . $this->city;
     }
 }
 
-class Address {
-    use Country, City;
+class Address
+{
+    use City, Country{
+        City::getName insteadof Country;
+        City::getName as getCityName;
+        Country::getName as getCountryName;
+    }
 
     private $country;
     private $city;
 
-    public function __construct($country, $city) {
+    public function __construct($country, $city)
+    {
         $this->country = $country;
         $this->city = $city;
     }
 
-    public function getFullAddress() {
+    public function getFullAddress()
+    {
         return $this->getCountryName() . ', ' . $this->getCityName();
     }
 }
